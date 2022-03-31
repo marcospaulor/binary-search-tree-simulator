48ad8c593d295e818fc28b0edb399915c3de6fda
Marcos Paulo <marcos_paulo.rodrigues@hotmail.com>
2022-03-31 01:55:08 -0300

    Projeto base

---
+++ scripts/sketch.js
@@ -0,0 +1,25 @@
+/* Resetar Navegador */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+/* Variáveis */
+:root{
+    --purple: #3204ff
+}
+
+header{
+    background-color: var(--purple);
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+}
+h1{
+    font-size: 2em;
+    font-weight: bold;
+    color: #fff;
+}
