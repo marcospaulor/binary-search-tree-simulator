d2c069d7d0f49fc6434b82dc2fc2201b0aaabec5
Gabriel Pistore <72265397+gabrielPistore@users.noreply.github.com>
2022-04-04 19:08:56 -0300

    updated sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,18 +1,12 @@
-function setup(){
-    // create a canvas black with a text white on the center
-    createCanvas(windowWidth, windowHeight);
-    background(0);
-    fill(255);
-    textSize(32);
-    textAlign(CENTER);
-    text("Click to draw", width/2, height/2);
+let binaryTree = new BinaryTree();
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(202, 240, 248);
+  for (let i = 0; i < 30; i++) {
+    binaryTree.insert(floor(random(0, 100)));
+  }
+  binaryTree.drawTree();
 }
 
-function draw(){
-    // draw a circle when mouse is pressed and random color
-    if(mouseIsPressed){
-        fill(random(255), random(255), random(255));
-        circle(mouseX, mouseY, random(100), random(100));
-    }
-
-}
+function draw() {}
